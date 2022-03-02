bf871b6ab47e7cf9874c8b2d3c1bd31a09e22c33
Isaias Carvalho <isaias.carvalho@iteris.com.br>
2022-03-02 19:43:27 -0400

    [fix] remove useless icons

--- lib/data/utils/app_assets.dart
+++ lib/data/utils/app_assets.dart
@@ -4,14 +4,4 @@ import 'package:get/get.dart';
 
 mixin AppAssets {
   static final SvgPicture andrewsFaceIcon = SvgPicture.asset('assets/images/andrews_face_icon.svg');
-
-
-  static final SvgPicture bookAndPencilIconDark = SvgPicture.asset('assets/images/book_pencil_dark.svg');
-  static final SvgPicture bookAndPencilIconLight = SvgPicture.asset('assets/images/book_and_pencil_light.svg');
-
-  static final SvgPicture hollyBibleIconDark = SvgPicture.asset('assets/images/holly_bible_dark.svg');
-  static final SvgPicture hollyBibleIconLight = SvgPicture.asset('assets/images/holy_bible_light.svg');
-
-  static final SvgPicture speechToTextIconDark = SvgPicture.asset('assets/images/speech_to_text_dark.svg');
-  static final SvgPicture speechToTextIconLight = SvgPicture.asset('assets/images/speech_to_text_light.svg');
 }
